5fab5a6d92603e45289da66a381459c2fc9e33a4
thamim ansari <thamimansari1626@gmail.com>
2023-11-05 22:28:31 +0000

    modify 1

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const bcrypt = require("bcrypt");
+
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+app.use(express.json());
+const path = require("path");
+const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
+let db = null;
+
+const initializeServerAndDb = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server 3000 starting.........");
+    });
+  } catch (e) {
+    process.exit(1);
+    console.log("server Fail -----");
+  }
+};
+
+initializeServerAndDb();
+
+app.get("/login", (request, response) => {
+  const { password } = request.params;
+  const hashedPassword = bcrypt.hash(password);
+  response.send(hashedPassword);
+});
